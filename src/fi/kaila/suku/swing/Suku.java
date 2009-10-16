5c1cb4079558c1d2c160f23913d8d65da0994fa8
Kaarlo Kaila <kaarlo.kaila@iki.fi>
2009-10-16 15:35:36 +0000

    replaced property files with excel that has macro to generate the property files. version 30

--- src/fi/kaila/suku/swing/Suku.java
+++ src/fi/kaila/suku/swing/Suku.java
@@ -168,7 +168,7 @@ public class Suku extends JFrame implements ActionListener,ComponentListener,
 	/**
 	 * Application version
 	 */
-	public static final String sukuVersion="11.-3.0026";
+	public static final String sukuVersion="11.-3.0030";
 	/**
 	 * Server version
 	 */
