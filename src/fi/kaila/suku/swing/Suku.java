1141e0c3bd43c87df89b62be02a856d582b405a6
Kaarlo Kaila <kaarlo.kaila@iki.fi>
2009-10-16 16:00:56 +0000

    version 31 that fixed property bug in version 30

--- src/fi/kaila/suku/swing/Suku.java
+++ src/fi/kaila/suku/swing/Suku.java
@@ -168,7 +168,7 @@ public class Suku extends JFrame implements ActionListener,ComponentListener,
 	/**
 	 * Application version
 	 */
-	public static final String sukuVersion="11.-3.0030";
+	public static final String sukuVersion="11.-3.0031";
 	/**
 	 * Server version
 	 */
