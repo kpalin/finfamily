72abeb707fc60a564b8b5831b8ba9356e3e8c5dd
Kaarlo Kaila <kaarlo.kaila@iki.fi>
2009-10-20 12:05:55 +0000

    version 43

--- src/fi/kaila/suku/swing/Suku.java
+++ src/fi/kaila/suku/swing/Suku.java
@@ -161,7 +161,7 @@ public class Suku extends JFrame implements ActionListener, ComponentListener,
 	/**
 	 * Application version
 	 */
-	public static final String sukuVersion = "11.-3.0034";
+	public static final String sukuVersion = "11.-3.0043";
 	/**
 	 * Server version
 	 */
