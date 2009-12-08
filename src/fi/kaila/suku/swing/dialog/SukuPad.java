a8084c3f40e3c4dbd260bd3461082801adc98584
Kaarlo Kaila <kaarlo.kaila@iki.fi>
2009-12-08 07:25:55 +0000

    added menu functions to SukuPad

--- src/fi/kaila/suku/swing/dialog/SukuPad.java
+++ src/fi/kaila/suku/swing/dialog/SukuPad.java
@@ -4,15 +4,20 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+import java.io.IOException;
+import java.io.OutputStream;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+import fi.kaila.suku.swing.Suku;
 import fi.kaila.suku.util.Resurses;
 
 /**
@@ -29,6 +34,7 @@ public class SukuPad extends JDialog implements ActionListener {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private JTextArea txtArea=null;
 	/**
 	 * @param owner
 	 * @param text
@@ -44,17 +50,20 @@ public class SukuPad extends JDialog implements ActionListener {
 		menubar.add(mFile);
 
 		JMenuItem save = new JMenuItem(Resurses.getString("FILE_SAVE_AS"));
+		save.addActionListener(this);
+		save.setActionCommand("SAVE_AS");
 		mFile.add(save);
 		JMenuItem print = new JMenuItem(Resurses.getString("FILE_PRINT"));
-
+		print.addActionListener(this);
+		print.setActionCommand("PRINT");
 		mFile.add(print);
 		JMenuItem cl = new JMenuItem(Resurses.getString("CLOSE"));
 		cl.addActionListener(this);
 		cl.setActionCommand("CLOSE");
 		mFile.add(cl);
 
-		JTextArea txt = new JTextArea(text);
-		JScrollPane sc = new JScrollPane(txt);
+		txtArea = new JTextArea(text);
+		JScrollPane sc = new JScrollPane(txtArea);
 		getContentPane().add(sc);
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 		setBounds(d.width / 2 - 300, d.height / 2 - 200, 600, 400);
@@ -68,6 +77,38 @@ public class SukuPad extends JDialog implements ActionListener {
 			return;
 		if (cmd.equals("CLOSE")) {
 			setVisible(false);
+		} else if (cmd.equals("PRINT")){
+			try {
+				txtArea.print();
+			} catch (PrinterException e) {
+				JOptionPane.showMessageDialog(this, Resurses
+						.getString("IMPORT_GEDCOM")
+						+ ":" + e.getMessage());
+			}
+		} else if (cmd.equals("SAVE_AS")){
+			boolean isFile = Suku.kontroller.createLocalFile("txt");
+			if (isFile) {
+				OutputStream fos = Suku.kontroller.getOutputStream();
+			
+				String tekst;
+				if (java.io.File.pathSeparatorChar == ';') {
+					tekst = txtArea.getText().replaceAll("\n", "\r\n");
+				} else {
+					tekst = txtArea.getText();
+				}
+				byte[] buffi = tekst.getBytes();
+				try {
+					fos.write(buffi);
+					fos.close();
+				} catch (IOException e) {
+					JOptionPane.showMessageDialog(this, Resurses
+							.getString("IMPORT_GEDCOM")
+							+ ":" + e.getMessage());
+				}
+			
+			}
+			
+			
 		}
 	}
 
