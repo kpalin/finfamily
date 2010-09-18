6abad1e4d1e9d2d3059ab7006b69ea0bca492ebe
Kaarlo Kaila <kaarlo.kaila@iki.fi>
2010-09-18 11:45:40 +0000

    Modified default order for notices

--- resources/sql/finfamily.sql
+++ resources/sql/finfamily.sql
@@ -529,12 +529,7 @@ insert into sukusettings (settingtype,settingindex,settingname,settingvalue) val
 insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',2,'notice','CHR');
 insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',3,'notice','OCCU');
 insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',4,'notice','EDUC');
-insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',5,'notice','NOTE');
-insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',6,'notice','PHOT');
-insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',7,'notice','PHOTO');
-insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',8,'notice','IMMI');
-insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',9,'notice','EMIG');
-insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',10,'notice','RESI');
-insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',11,'notice','EVEN');
-insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',12,'notice','DEAT');
-insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',13,'notice','BURI');
+insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',5,'notice','DEAT');
+insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',6,'notice','BURI');
+insert into sukusettings (settingtype,settingindex,settingname,settingvalue) values ('order',7,'notice','RESI');
+
